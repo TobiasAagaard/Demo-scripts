88a2e9a1b27fb611f448e7a5b7eb0056396522d1
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-10-02 13:57:42 +0200

    CorrectCalc

---
+++ CorrectCalc.ps1
@@ -0,0 +1,18 @@
+﻿$filePath = 'C:\Users\tobia\OneDrive\Dokumenter\Datatekniker\Programmering\Powershell\Demo scripts\calc2.txt';
+
+$problems = Get-Content $filePath
+
+foreach ($problem in $problems) {
+    
+    $parts = $problem -split ' '
+    
+    $calculation = "$($parts[2]) $($parts[3]) $($parts[4])"
+    
+    $expectedResult = [int]$parts[0]
+  
+    $actualResult = Invoke-Expression $calculation
+
+    if ($actualResult -ne $expectedResult) {
+        Write-Output "Fejl i regnestykket: $problem (Forventet: $expectedResult, Faktisk: $actualResult)"
+    }
+}
