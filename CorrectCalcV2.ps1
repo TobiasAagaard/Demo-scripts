36eb3837f60f2ac651dff32fb9eecd5a8f88acbf
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-10-28 11:12:11 +0100

    Calcv2

---
+++ CorrectCalcV2.ps1
@@ -0,0 +1,25 @@
+﻿$filePath = 'C:\Users\tobia\OneDrive\Dokumenter\Datatekniker\Programmering\Powershell\Demo scripts\calc2.txt'
+
+$problems = get-content $filePath
+
+function CorrectCalc () {
+    
+    $splitString = $problems -split '';
+
+    $operator = $splitString[3]
+    [int]$number1 = $splitString[2]
+    [int]$number2 = $splitString[4]
+    [int]$sum = $splitString[0]
+
+    if ($operator -eq "+") {
+        $result = $number1 + $number2
+    } elseif ($operator -eq "-") {
+        $result = $number1 - $number2
+    } elseif ($operator -eq "/") {
+        $result = $number1 / $number2
+    } elseif ($operator -eq "*") {
+        $result = $number1 -eq $number2
+    }
+
+    
+}
