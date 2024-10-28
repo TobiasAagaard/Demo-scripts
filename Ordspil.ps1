dcc5b9c2ece1c35c065b550523e34a779ca54e16
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-10-28 11:40:41 +0100

    Update Ordspil.ps1

--- Ordspil.ps1
+++ Ordspil.ps1
@@ -1,9 +1,6 @@
 ﻿# Bed brugeren om en sætning
 $sætning = Read-Host "Skriv en sætning"
 
-# Udskriv sætningen baglæns
-$baglæns = -join ($sætning.ToCharArray() | ForEach-Object { $_ })[-1..0]
-Write-Host "`nSætningen baglæns: $baglæns"
 
 # Konverter til LEET speak
 $leetSætning = $sætning -replace 'a', '4' -replace 'e', '3' -replace 'i', '1' -replace 'o', '0' -replace 's', '5' -replace 't', '7'
