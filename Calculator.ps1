7cb3d73b6c14a3c2347a7a86cdde7c8a256494e9
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-10-28 11:41:57 +0100

    sqrt

--- Calculator.ps1
+++ Calculator.ps1
@@ -1,31 +1,37 @@
 ﻿function Calculator {
+    Write-Host "Dette er en lommeregner"
+    [float] $numb1 = Read-Host "Tast første tal"
+    $operation = Read-Host "Vælg din regneart (+, -, *, /, sqrt)"
+    $result = 0
 
-Write-Host "Dette er en lommeregner"
-[float] $numb1 = Read-Host "tast første tal"
-[float] $numb2 = Read-Host "tast andet tal"
-        $operation = Read-Host "Vælg dit regneart (+, -, *, /)"
-        $result = 0
-      
-      switch ($operation) {
+    if ($operation -ne "sqrt") {
+        [float] $numb2 = Read-Host "Tast andet tal"
+    }
+
+    switch ($operation) {
         "+" {$result = $numb1 + $numb2; break}
-
         "-" {$result = $numb1 - $numb2; break}
-
         "*" {$result = $numb1 * $numb2; break}
-
-        "/" {if ($numb2 -ne 0)   {
+        "/" {
+            if ($numb2 -ne 0) {
                 $result = $numb1 / $numb2
-        } else {
-            $result = "Fejl, du kan ikke dividere med nul!"
-        }; break} 
-
-        default {$result = "Du skal bruge en anden regneart(+, -, *, /)"}
-
+            } else {
+                $result = "Fejl, du kan ikke dividere med nul!"
+            }; break
         }
-      
+        "sqrt" {
+            if ($numb1 -ge 0) {
+                $result = [math]::Sqrt($numb1)
+            } else {
+                $result = "Fejl, du kan ikke tage kvadratroden af et negativt tal!"
+            }; break
+        }
+        default {
+            $result = "Du skal bruge en anden regneart (+, -, *, /, sqrt)"
+        }
+    }
 
-
-    Write-Host "$result"
+    Write-Host "Resultat: $result"
 }
 
 Calculator
