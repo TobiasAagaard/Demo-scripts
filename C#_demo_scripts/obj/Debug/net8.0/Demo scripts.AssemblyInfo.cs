4da252c81903c807d4fb448cf2311366d7384e17
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-11-01 11:51:16 +0100

    Det er spiller X tur

--- C#_demo_scripts/obj/Debug/net8.0/Demo scripts.AssemblyInfo.cs
+++ C#_demo_scripts/obj/Debug/net8.0/Demo scripts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo scripts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+354b25d442d7027d12a8185bfdd06d7bfe18ea1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c15fe9d97227a4ca4d113ff3c2eeba82b96959c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo scripts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo scripts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- C#_demo_scripts/tictactoe.cs
+++ C#_demo_scripts/tictactoe.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class TicTacToe {
+    static char[] board = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
+    static int player = 1;
+    static int choice; 
+    static int flag = 0;
+
+    static void Main() {
+        do {
+            if (player % 2 == 0) {
+                Console.WriteLine("Det er spiller 0 tur");
+            } else {
+                Console.WriteLine("Det er spiller X tur");
+            };
+        } while(flag == 1);
+    }
+}
