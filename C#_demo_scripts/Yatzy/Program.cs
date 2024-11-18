2863885aa3d4bb6fca250bbaf8af536238fb49ab
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-11-18 10:14:03 +0100

    Playing while loop

--- C#_demo_scripts/Yatzy/Program.cs
+++ C#_demo_scripts/Yatzy/Program.cs
@@ -3,5 +3,12 @@
 class Program {
     static void Main(string[] args) {
            Console.WriteLine("-- Welcome to Yatzy --");
+
+           bool playing = true;
+           int turn = 1;
+
+           while (playing) {
+            
+           }
     }
 }
