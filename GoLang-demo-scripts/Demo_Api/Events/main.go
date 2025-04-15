38682c4aec1aa94cbee21326b62ece359beaed8d
TobiasAagaard <Tobias909@live.dk>
2025-04-15 13:23:31 +0200

    routes setup

--- GoLang-demo-scripts/Demo_Api/Events/main.go
+++ GoLang-demo-scripts/Demo_Api/Events/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"example.com/events/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
-
+	routes.SetupRoutes(server)
 	server.Run("8080")
 }
